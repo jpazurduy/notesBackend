ebdc6e19dc08a1f5ebccd46f71b3b9bda0c9d4f0
Jorge Azurduy <jpa.azurduy@gmail.com>
2026-05-17 18:53:52 -0400

    adding until part 5 and 6

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
+
+
 
 let notes = [
   {
@@ -35,36 +39,33 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
-
-// app.get('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   const note = notes.find(note => note.id === id)
-//   response.json(note)
-// })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
   })
-   if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-
 
 
 const generateId = () => {
@@ -76,26 +77,93 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  console.log(body.content)
+
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+// app.get('/api/notes', (request, response) => {
+//   response.json(notes)
+// })
+
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = request.params.id
+//   const note = notes.find(note => note.id === id)
+//   response.json(note)
+// })
+
+
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const note = notes.find(note => {
+//     console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+//     return note.id === id
+//   })
+//    if (note) {
+//     response.json(note)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
+
+// app.post('/api/notes', (request, response) => {
+//   const body = request.body
+//   console.log(body.content)
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: Boolean(body.important) || false,
+//     id: generateId(),
+//   }
+
+//   notes = notes.concat(note)
+
+//   response.json(note)
+// })
+
+// app.delete('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   notes = notes.filter(note => note.id !== id)
+
+//   response.status(204).end()
+// })
+
